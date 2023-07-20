f97cb0c454de100a2d14f21dbbbb711c28eaeb89
shiryukuroda <200447028@ccmailg.meijo-u.ac.jp>
2023-07-20 12:10:41 +0900

    first commit

---
+++ src/online_node.cpp
@@ -0,0 +1,10 @@
+#include "online.hpp"
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<Online>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
